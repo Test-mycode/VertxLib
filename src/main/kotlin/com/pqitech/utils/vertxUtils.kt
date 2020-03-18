7de58900a40ac19101d7c267c302582a2b1da431
dushibaiyu <dushibaiyu@foxmail.com>
2020-03-18 15:53:54 +0800

    vertx log

--- src/main/kotlin/com/pqitech/utils/vertxUtils.kt
+++ src/main/kotlin/com/pqitech/utils/vertxUtils.kt
@@ -9,4 +9,7 @@ import io.netty.util.internal.logging.Log4J2LoggerFactory
 
 fun Vertx.launch(handle : suspend ()-> Unit) = CoroutineScope(this.dispatcher()).launch{handle()}
 
-fun nettyUseLog4j() =  InternalLoggerFactory.setDefaultFactory(Log4J2LoggerFactory.INSTANCE)
+fun nettyUseLog4j() {
+  System.setProperty("vertx.logger-delegate-factory-class-name","io.vertx.core.logging.Log4j2LogDelegateFactory")
+  InternalLoggerFactory.setDefaultFactory(Log4J2LoggerFactory.INSTANCE)
+}
