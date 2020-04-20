eeb9f0a5b8f5aeb0c556b076189f70ba2cd36013
dushibaiyu <dushibaiyu@foxmail.com>
2020-04-20 17:41:57 +0800

    0.0.5

---
+++ src/main/kotlin/com/pqitech/vertx/VertxApp.kt
@@ -0,0 +1,51 @@
+package com.pqitech.vertx
+
+import com.pqitech.utils.parseArgs
+import io.vertx.core.DeploymentOptions
+import io.vertx.core.Verticle
+import io.vertx.core.Vertx
+import io.vertx.core.VertxOptions
+import io.vertx.core.json.JsonObject
+import io.vertx.kotlin.core.deployVerticleAwait
+import java.util.function.Supplier
+
+abstract class VertxApp {
+  private lateinit var option_: JsonObject
+  private var poolSize_ : Int = 2
+  private var workPoolSize_ : Int = 2
+  private lateinit var vertx_: Vertx;
+
+  val vertx get() = vertx_
+  val startOptions get() = option_
+  val eventLoopSize get() = poolSize_
+  val workPoolSize get() = workPoolSize_
+
+  fun start(args : Array<String>, name : String = "Pqitech Vertx App"){
+    val option = parseArgs(args, name)
+    start(option)
+  }
+
+  fun start(option: JsonObject)
+  {
+    this.option_ = option
+    val optionVertx = VertxOptions(option.getJsonObject("vertx", JsonObject()))
+    this.poolSize_ = optionVertx.eventLoopPoolSize
+    this.workPoolSize_ = optionVertx.workerPoolSize
+    this.vertx_ = Vertx.vertx(optionVertx)
+    doStart()
+  }
+
+  protected abstract fun doStart() ;
+
+  suspend fun deployVerticleConfigAwait(size : Int ,hander : ()->Verticle){
+    val deploymentOptions = DeploymentOptions()
+    deploymentOptions.instances = size
+    deployVerticleConfigAwait(deploymentOptions,hander)
+  }
+
+  suspend fun deployVerticleConfigAwait(deploymentOptions : DeploymentOptions ,hander : ()->Verticle){
+    deploymentOptions.config = startOptions
+    vertx.deployVerticleAwait(Supplier<Verticle> { hander() },deploymentOptions)
+  }
+}
+
---
+++ src/main/kotlin/com/pqitech/vertx/WebVerticle.kt
@@ -0,0 +1,65 @@
+package com.pqitech.vertx
+
+import com.pqitech.exception.ErrorCodeException
+import com.pqitech.utils.endJson
+import io.vertx.core.http.HttpServer
+import io.vertx.core.http.HttpServerOptions
+import io.vertx.core.json.JsonObject
+import io.vertx.ext.web.Router
+import io.vertx.ext.web.RoutingContext
+import io.vertx.kotlin.coroutines.CoroutineVerticle
+import io.vertx.kotlin.coroutines.await
+import org.apache.logging.log4j.LogManager
+
+abstract class AbstractWebVerticle  : CoroutineVerticle() {
+  private val log_ = LogManager.getLogger("com.pqitech.vertx.WebVerticle")
+  private lateinit var server : HttpServer
+  private lateinit var router : Router
+
+  override suspend fun start() {
+    server = vertx.createHttpServer(httpOptions())
+    router = Router.router(vertx)
+    router.route().failureHandler(::failureHandler)
+    doInit()
+    server.requestHandler(router).listen().await()
+  }
+
+  protected abstract suspend fun  doInit()
+
+  override suspend fun stop() {
+    server.close().await()
+  }
+
+  private fun httpOptions() : HttpServerOptions {
+    val options = HttpServerOptions(config.getJsonObject("web", JsonObject()))
+    options.setReuseAddress(true).isReusePort = true
+    return  options
+  }
+
+  private fun failureHandler(context: RoutingContext)
+  {
+    handleException(context,context.failure(),404)
+  }
+
+  fun handleException(context: RoutingContext, e : Throwable?, status : Int = 500, msg : String = "")
+  {
+    val ret = JsonObject()
+    ret.put("status",status);
+    ret.put("error", status)
+    if(msg.isEmpty())
+      ret.put("message", e?.message ?: "未知错误")
+    else
+      ret.put("message", msg)
+    ret.put("data", JsonObject())
+    if(e != null){
+      if(e is ErrorCodeException){
+        log_.debug("catch ErrorCodeException : ",e);
+        ret.put("status", e.errorCode)
+      } else {
+        e.printStackTrace()
+        log_.warn("catch error exception : ",e);
+      }
+    }
+    context.response().setStatusCode(200).endJson(ret)
+  }
+}
